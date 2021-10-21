21d61991cea91240e168f6d1c4f8314257fd8fcc
JulienLEUILLIER <julien.leuillier@gmail.com>
2021-10-21 21:55:00 +0200

    Good base for final result

--- app/js/script.js
+++ app/js/script.js
@@ -1,5 +1,24 @@
 const submitBtn = document.querySelector('.card__submit');
+const inputField = document.querySelector('.card__input');
+const formElement = document.querySelector('.card__form');
 
-submitBtn.addEventListener('click', () => {
-   submitBtn.classList.toggle('clicked');
-})
+submitBtn.addEventListener('click', (event) => {
+   event.preventDefault();
+   if (inputField.value === '') {
+      formElement.classList.add('active');
+   }
+   const timer = setInterval(() => {
+      formElement.classList.remove('active');
+      clearInterval(timer);
+   }, 3000);
+});
+
+inputField.addEventListener('focus', () => {
+   inputField.style.color = 'black';
+   inputField.setAttribute('placeholder', '');
+});
+
+inputField.addEventListener('focusout', () => {
+   inputField.style.color = 'hsl(0, 36%, 70%)';
+   inputField.setAttribute('placeholder', 'Email Address');
+});
